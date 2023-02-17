779c15ffd760fe4680e91fb88965ba59ebed21ea
Fabien Morrow <fabien.morrow@protonmail.com>
2023-02-17 18:34:17 +0100

    mwe

--- src/DamnValuableToken.sol
+++ src/DamnValuableToken.sol
@@ -8,8 +8,5 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
  */
 contract DamnValuableToken is ERC20 {
-    constructor() ERC20("DamnValuableToken", "DVT") {
-        bytes memory payload = abi.encodeWithSelector(ERC20.transfer.selector);
-        _mint(msg.sender, type(uint256).max);
-    }
+    constructor() ERC20("DamnValuableToken", "DVT") {}
 }
